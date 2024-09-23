f4b1398e9725ffe5e475660c43b83776197d7e59
Nicolas LECLERCQ <common.nicolasleclercq@gmail.com>
2024-09-23 23:45:17 +0200

    feature(API): init api post and get

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,1 +1,27 @@
 const baseUrl = process.env.API_URL;
+
+class EuroMillionsAPI {
+
+	public async Get<Type>(url: string): Promise<Type | Error> {
+		return fetch(`${baseUrl}${url}`, {
+			method: 'GET',
+		}).then((response: Response) => {
+			return response.json();
+		}).catch((error: Error) => {
+			return error;
+		});
+	}
+
+	public async Post<Type>(url: string, body: Type): Promise<Response | Error> {
+		return fetch(`${baseUrl}${url}`, {
+			method: 'POST',
+			body: JSON.stringify(body),
+		}).then((response: Response) => {
+			return response.json();
+		}).catch((error: Error) => {
+			return error;
+		});
+	}
+}
+
+export const euroMillionsAPI:EuroMillionsAPI = new EuroMillionsAPI();
