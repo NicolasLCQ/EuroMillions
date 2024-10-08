fa9317b949ca506f5cf1112d5eb1bd4db3759554
Nicolas LECLERCQ <common.nicolasleclercq@gmail.com>
2024-10-08 02:24:06 +0200

    fix(routes):  WebApplciation => IEndpointRouteBuilder for routes

--- server/EuroMillions.API/Routes/UploadRoutes.cs
+++ server/EuroMillions.API/Routes/UploadRoutes.cs
@@ -2,7 +2,7 @@ namespace EuroMillions.API.Routes;
 
 public static class UploadRoutes
 {
-    public static void UseUploadRoutes(this WebApplication app)
+    public static void UseUploadRoutes(this IEndpointRouteBuilder app)
     {
         RouteGroupBuilder uploadGroup = app.MapGroup("/upload");
 
