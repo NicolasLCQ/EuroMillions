3a475f29dfda848156f999e8e596e55324337180
Nicolas LECLERCQ <common.nicolasleclercq@gmail.com>
2025-01-11 17:33:42 +0100

    feat(ErrorHandler): Replace return by throw Exception

--- server/EuroMillions.API/Resources/UploadRessource.cs
+++ server/EuroMillions.API/Resources/UploadRessource.cs
@@ -12,16 +12,14 @@ public class UploadRessource(IUploadServices uploadServices)
     {
         if (!files.Any())
         {
-            //TODO : throw error instead ? and delegate return to Error Hnadlers only
             throw new ApplicationException("No files provided.");
-            return Results.BadRequest("No files provided");
         }
 
         HashSet<string> fileNames = new();
 
         if (files.Any(file => fileNames.Add(file.FileName) == false))
         {
-            return Results.BadRequest("Duplicate files are not accepted");
+            throw new ApplicationException("Duplicate files are not accepted");
         }
 
         List<UploadResultModel> uploadResult = await uploadServices.UploadDrawsFromCsvFilesAsync(files.Select(f => f.ToUploadFileModel()));
