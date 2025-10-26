1d32648c8a8db8106098926c2f305f7a3fadd748
Nicolas LECLERCQ <common.nicolasleclercq@gmail.com>
2025-10-27 00:24:46 +0100

    feat(devtools): add React Query Devtools to EuroMillionsApp

--- client/src/App/EuroMillionsApp.tsx
+++ client/src/App/EuroMillionsApp.tsx
@@ -1,10 +1,12 @@
 import './const.css';
 import QueryProvider from "./Providers/QueryProvider/QueryProvider.tsx";
 import EuroMillionsRouter from "./Router/EuroMillionsRouter.tsx";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 const EuroMillionsApp = () => (
 	<QueryProvider>
 		<EuroMillionsRouter/>
+		<ReactQueryDevtools initialIsOpen={false} />
 	</QueryProvider>
 )
 
