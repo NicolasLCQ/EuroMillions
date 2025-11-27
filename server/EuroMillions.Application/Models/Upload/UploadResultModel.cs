173e57b81ec5238618ab73dcca7c3cc3137b0a3d
Nicolas LECLERCQ <common.nicolasleclercq@gmail.com>
2025-11-27 01:18:17 +0100

    refactor(models): add TODO for refining rejected file details in `UploadResultModel`

--- server/EuroMillions.Application/Models/Upload/UploadResultModel.cs
+++ server/EuroMillions.Application/Models/Upload/UploadResultModel.cs
@@ -2,6 +2,7 @@ namespace EuroMillions.Application.Models;
 
 public class UploadResultModel
 {
+    //TODO: change : nbFilesAdded, fileOfRejected?{fileName,drawId,Reason}
     public string FileName { get; set; }
     public List<Draw> AcceptedDraws { get; set; }
     public List<Draw> RejectedDraws { get; set; }
