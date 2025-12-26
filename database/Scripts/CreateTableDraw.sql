a9708f4c5d381f328d4b79bc713efab2cdf8b034
Nicolas LECLERCQ <common.nicolasleclercq@gmail.com>
2025-12-26 01:26:50 +0100

    refactor(database): refacto sql to feat sqlite

--- database/Scripts/CreateTableDraw.sql
+++ database/Scripts/CreateTableDraw.sql
@@ -1,16 +1,14 @@
-USE DB_EUROMILLIONS;
-
 CREATE TABLE IF NOT EXISTS T_DRAW
 (
-    ID               int      PRIMARY KEY not null AUTO_INCREMENT,
-    YEAR_DRAW_NUMBER int      not null,
-    DRAW_DATE             DATETIME not null,
-    BALL_ONE         int      not null,
-    BALL_TWO         int      not null,
-    BALL_THREE       int      not null,
-    BALL_FOUR        int      not null,
-    BALL_FIVE        int      not null,
-    STAR_ONE         int      not null,
-    STAR_TWO         int      not null,
+    ID               INTEGER   PRIMARY KEY AUTOINCREMENT NOT NULL,
+    YEAR_DRAW_NUMBER INT       not null,
+    DRAW_DATE        DATETIME  not null,
+    BALL_ONE         INT       not null,
+    BALL_TWO         INT       not null,
+    BALL_THREE       INT       not null,
+    BALL_FOUR        INT       not null,
+    BALL_FIVE        INT       not null,
+    STAR_ONE         INT       not null,
+    STAR_TWO         INT       not null,
     CONSTRAINT T_DRAW_YEAR_DRAW_NUMBER_uindex UNIQUE (YEAR_DRAW_NUMBER)
 );
