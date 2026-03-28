115623a4fdfa1e1a1c80829441070df491f0f4e8
Nicolas LECLERCQ <common.nicolasleclercq@gmail.com>
2026-03-28 20:09:25 +0100

    refactor(config): update server settings and simplify tsconfig paths
    - Enabled auto-open browser on server start in `vite.config.ts`.
    - Streamlined path aliases in `tsconfig.json` for cleaner structure.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
 	server: {
-		port: 3000,
+		open: true,
+		port: 3000
 	},
 })
