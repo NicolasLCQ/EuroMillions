5c705fb1c95fc38964189ddcd9a9ce6c71505c77
Nicolas LECLERCQ <common.nicolasleclercq@gmail.com>
2026-03-28 23:37:26 +0100

    refactor(components): reorganize `ClickableComponentWrapper` and streamline imports
    - Moved `ClickableComponentWrapper` to a dedicated folder for better structure.
    - Updated imports in related modules to reflect the new structure.
    - Adjusted tsconfig path mappings for consistency.
    - Upgraded dependencies in `package-lock.json`.

---
+++ client/src/Shared/components/ClickableComponents/index.ts
@@ -0,0 +1,1 @@
+export {default as ClickableComponentWrapper} from "./ClickableComponentWrapper";
--- client/src/Shared/components/TextComponents/index.ts
+++ client/src/Shared/components/TextComponents/index.ts
@@ -1,4 +1,3 @@
-export { default as ClickableComponentWrapper } from "./ClickableComponentWrapper";
 export * from "./PageTitleComponent";
 export * from "./TextComponent";
 export * from "./TitleComponent";
