6858fe126212839657deaddaf0e71fb9c61acc2e
Nicolas LECLERCQ <common.nicolasleclercq@gmail.com>
2026-04-12 03:23:18 +0200

    refactor(draws): remove `AddDrawsFromDrawFileModelsAsync` and centralize deduplication logic
    - Deleted `AddDrawsFromDrawFileModelsAsync` and associated interface method.
    - Moved draw deduplication logic to the new `AddDrawsWithDeduplicationAsync` private method.
    - Updated usages in `DrawUseCases` to use the centralized deduplication logic.
    - Renamed `ToT_DRAWEntity` to `ToDRAWEntity` for consistency.
    - Changed `FileName` in `UploadFileResultModel` to be a required property.

--- server/EuroMillions.Application/Interfaces/Infrastructure/Repositories/IDrawRepository.cs
+++ server/EuroMillions.Application/Interfaces/Infrastructure/Repositories/IDrawRepository.cs
@@ -1,11 +1,9 @@
 using EuroMillions.Application.Models;
-using EuroMillions.Application.Models.Upload;
 
 namespace EuroMillions.Application.Interfaces.Infrastructure.Repositories;
 
 public interface IDrawRepository
 {
-    Task<List<UploadResultSimpleModel>> AddDrawsFromDrawFileModelsAsync(List<DrawFileModel> drawFileModels);
     Task<List<Draw>> GetAllDrawsAsync();
     Task<Draw?> GetLastDrawAsync();
     Task AddDrawsAsync(List<Draw> draws);
