49ac68e32a682a28c78a8e997b1c417fbe356011
Nicolas LECLERCQ <common.nicolasleclercq@gmail.com>
2026-04-26 18:14:11 +0200

    feat(database): add draw metadata and payout columns to T_DRAW
    - Added new columns to `T_DRAW` for draw metadata (e.g., draw day, forclusion date) and EuroMillions/Étoile Plus payout details.
    - Updated `EuroMillionsDbContext` and `T_DRAW` entity class to reflect schema changes.
    - Introduced a migration script (`2_UpdateTableDraw_AddDrawDetails.sql`) for updating the database schema.
    - Added a script (`Scaffold-Database.ps1`) to automate Entity Framework Core scaffolding for SQLite.

--- server/EuroMillions.Infrastructure/Context/EuroMillionsDbContext.cs
+++ server/EuroMillions.Infrastructure/Context/EuroMillionsDbContext.cs
@@ -1,39 +1,38 @@
-﻿using EuroMillions.Infrastructure.Entities;
-
+﻿using System;
+using System.Collections.Generic;
+using EuroMillions.Infrastructure.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace EuroMillions.Infrastructure.Context;
 
 public partial class EuroMillionsDbContext : DbContext
 {
-    public EuroMillionsDbContext() {}
-
     public EuroMillionsDbContext(DbContextOptions<EuroMillionsDbContext> options)
-        : base(options) {}
+        : base(options)
+    {
+    }
 
     public virtual DbSet<T_DRAW> T_DRAWs { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<T_DRAW>(entity =>
-            {
-                entity.ToTable("T_DRAW");
+        {
+            entity.ToTable("T_DRAW");
 
-                entity.HasIndex(e => e.YEAR_DRAW_NUMBER, "IX_T_DRAW_YEAR_DRAW_NUMBER").IsUnique();
+            entity.HasIndex(e => e.YEAR_DRAW_NUMBER, "IX_T_DRAW_YEAR_DRAW_NUMBER").IsUnique();
 
-                entity.Property(e => e.BALL_FIVE).HasColumnType("INT");
-                entity.Property(e => e.BALL_FOUR).HasColumnType("INT");
-                entity.Property(e => e.BALL_ONE).HasColumnType("INT");
-                entity.Property(e => e.BALL_THREE).HasColumnType("INT");
-                entity.Property(e => e.BALL_TWO).HasColumnType("INT");
-                entity.Property(e => e.DRAW_DATE).HasColumnType("DATETIME");
-                entity.Property(e => e.STAR_ONE).HasColumnType("INT");
-                entity.Property(e => e.STAR_TWO).HasColumnType("INT");
-                entity.Property(e => e.YEAR_DRAW_NUMBER).HasColumnType("INT");
-            }
-        );
+            entity.Property(e => e.BALL_FIVE).HasColumnType("INT");
+            entity.Property(e => e.BALL_FOUR).HasColumnType("INT");
+            entity.Property(e => e.BALL_ONE).HasColumnType("INT");
+            entity.Property(e => e.BALL_THREE).HasColumnType("INT");
+            entity.Property(e => e.BALL_TWO).HasColumnType("INT");
+            entity.Property(e => e.DRAW_DATE).HasColumnType("DATETIME");
+            entity.Property(e => e.FORCLUSION_DATE).HasColumnType("DATETIME");
+            entity.Property(e => e.STAR_ONE).HasColumnType("INT");
+            entity.Property(e => e.STAR_TWO).HasColumnType("INT");
+            entity.Property(e => e.YEAR_DRAW_NUMBER).HasColumnType("INT");
+        });
 
         OnModelCreatingPartial(modelBuilder);
     }
--- server/EuroMillions.Infrastructure/Entities/T_DRAW.cs
+++ server/EuroMillions.Infrastructure/Entities/T_DRAW.cs
@@ -24,4 +24,140 @@ public partial class T_DRAW
     public int STAR_ONE { get; set; }
 
     public int STAR_TWO { get; set; }
+
+    public string? DRAW_DAY { get; set; }
+
+    public DateTime? FORCLUSION_DATE { get; set; }
+
+    public string? DRAW_NUMBER_IN_CYCLE { get; set; }
+
+    public string? WINNING_BALLS_IN_ASCENDING_ORDER { get; set; }
+
+    public string? WINNING_STARS_IN_ASCENDING_ORDER { get; set; }
+
+    public string? JOKER_PLUS_NUMBER { get; set; }
+
+    public string? MY_MILLION_NUMBER { get; set; }
+
+    public string? EXCEPTIONAL_EURO_MILLIONS_DRAW_NUMBER { get; set; }
+
+    public string? CURRENCY { get; set; }
+
+    public int? RANK_1_EURO_MILLIONS_WINNERS_FRANCE { get; set; }
+
+    public int? RANK_1_EURO_MILLIONS_WINNERS_EUROPE { get; set; }
+
+    public double? RANK_1_EURO_MILLIONS_PRIZE { get; set; }
+
+    public int? RANK_2_EURO_MILLIONS_WINNERS_FRANCE { get; set; }
+
+    public int? RANK_2_EURO_MILLIONS_WINNERS_EUROPE { get; set; }
+
+    public double? RANK_2_EURO_MILLIONS_PRIZE { get; set; }
+
+    public int? RANK_3_EURO_MILLIONS_WINNERS_FRANCE { get; set; }
+
+    public int? RANK_3_EURO_MILLIONS_WINNERS_EUROPE { get; set; }
+
+    public double? RANK_3_EURO_MILLIONS_PRIZE { get; set; }
+
+    public int? RANK_4_EURO_MILLIONS_WINNERS_FRANCE { get; set; }
+
+    public int? RANK_4_EURO_MILLIONS_WINNERS_EUROPE { get; set; }
+
+    public double? RANK_4_EURO_MILLIONS_PRIZE { get; set; }
+
+    public int? RANK_5_EURO_MILLIONS_WINNERS_FRANCE { get; set; }
+
+    public int? RANK_5_EURO_MILLIONS_WINNERS_EUROPE { get; set; }
+
+    public double? RANK_5_EURO_MILLIONS_PRIZE { get; set; }
+
+    public int? RANK_6_EURO_MILLIONS_WINNERS_FRANCE { get; set; }
+
+    public int? RANK_6_EURO_MILLIONS_WINNERS_EUROPE { get; set; }
+
+    public double? RANK_6_EURO_MILLIONS_PRIZE { get; set; }
+
+    public int? RANK_7_EURO_MILLIONS_WINNERS_FRANCE { get; set; }
+
+    public int? RANK_7_EURO_MILLIONS_WINNERS_EUROPE { get; set; }
+
+    public double? RANK_7_EURO_MILLIONS_PRIZE { get; set; }
+
+    public int? RANK_8_EURO_MILLIONS_WINNERS_FRANCE { get; set; }
+
+    public int? RANK_8_EURO_MILLIONS_WINNERS_EUROPE { get; set; }
+
+    public double? RANK_8_EURO_MILLIONS_PRIZE { get; set; }
+
+    public int? RANK_9_EURO_MILLIONS_WINNERS_FRANCE { get; set; }
+
+    public int? RANK_9_EURO_MILLIONS_WINNERS_EUROPE { get; set; }
+
+    public double? RANK_9_EURO_MILLIONS_PRIZE { get; set; }
+
+    public int? RANK_10_EURO_MILLIONS_WINNERS_FRANCE { get; set; }
+
+    public int? RANK_10_EURO_MILLIONS_WINNERS_EUROPE { get; set; }
+
+    public double? RANK_10_EURO_MILLIONS_PRIZE { get; set; }
+
+    public int? RANK_11_EURO_MILLIONS_WINNERS_FRANCE { get; set; }
+
+    public int? RANK_11_EURO_MILLIONS_WINNERS_EUROPE { get; set; }
+
+    public double? RANK_11_EURO_MILLIONS_PRIZE { get; set; }
+
+    public int? RANK_12_EURO_MILLIONS_WINNERS_FRANCE { get; set; }
+
+    public int? RANK_12_EURO_MILLIONS_WINNERS_EUROPE { get; set; }
+
+    public double? RANK_12_EURO_MILLIONS_PRIZE { get; set; }
+
+    public int? RANK_13_EURO_MILLIONS_WINNERS_FRANCE { get; set; }
+
+    public int? RANK_13_EURO_MILLIONS_WINNERS_EUROPE { get; set; }
+
+    public double? RANK_13_EURO_MILLIONS_PRIZE { get; set; }
+
+    public int? RANK_1_ETOILE_PLUS_WINNERS { get; set; }
+
+    public double? RANK_1_ETOILE_PLUS_PRIZE { get; set; }
+
+    public int? RANK_2_ETOILE_PLUS_WINNERS { get; set; }
+
+    public double? RANK_2_ETOILE_PLUS_PRIZE { get; set; }
+
+    public int? RANK_3_ETOILE_PLUS_WINNERS { get; set; }
+
+    public double? RANK_3_ETOILE_PLUS_PRIZE { get; set; }
+
+    public int? RANK_4_ETOILE_PLUS_WINNERS { get; set; }
+
+    public double? RANK_4_ETOILE_PLUS_PRIZE { get; set; }
+
+    public int? RANK_5_ETOILE_PLUS_WINNERS { get; set; }
+
+    public double? RANK_5_ETOILE_PLUS_PRIZE { get; set; }
+
+    public int? RANK_6_ETOILE_PLUS_WINNERS { get; set; }
+
+    public double? RANK_6_ETOILE_PLUS_PRIZE { get; set; }
+
+    public int? RANK_7_ETOILE_PLUS_WINNERS { get; set; }
+
+    public double? RANK_7_ETOILE_PLUS_PRIZE { get; set; }
+
+    public int? RANK_8_ETOILE_PLUS_WINNERS { get; set; }
+
+    public double? RANK_8_ETOILE_PLUS_PRIZE { get; set; }
+
+    public int? RANK_9_ETOILE_PLUS_WINNERS { get; set; }
+
+    public double? RANK_9_ETOILE_PLUS_PRIZE { get; set; }
+
+    public int? RANK_10_ETOILE_PLUS_WINNERS { get; set; }
+
+    public double? RANK_10_ETOILE_PLUS_PRIZE { get; set; }
 }
