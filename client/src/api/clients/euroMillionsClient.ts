6203890fb2b7620ba74951bd95e5b596b5dce676
Nicolas LECLERCQ <common.nicolasleclercq@gmail.com>
2026-05-29 20:08:41 +0200

    refactor(fdjclient): pass url through config

--- client/src/api/clients/euroMillionsClient.ts
+++ client/src/api/clients/euroMillionsClient.ts
@@ -1,6 +1,6 @@
 import {config} from 'config';
 
-const API_BASE_URL = config.API_URL.replace(/\/$/, '');
+const API_BASE_URL = config.EUROMILLIONS_API_URL.replace(/\/$/, '');
 
 async function euroMillionsFetch(endpoint: string, init?: RequestInit): Promise<Response> {
   return await fetch(`${API_BASE_URL}${endpoint}`, init);
