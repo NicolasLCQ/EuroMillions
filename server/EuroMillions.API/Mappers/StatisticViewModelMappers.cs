8c13679773977530d529c17a716160c9f86d109e
NicolasLCQ <pro.nicolasleclercq@gmail.com>
2026-07-15 02:59:28 +0200

    refactor(mappers): update ToStatisticViewModel to use generic type constraint

--- server/EuroMillions.API/Mappers/StatisticViewModelMappers.cs
+++ server/EuroMillions.API/Mappers/StatisticViewModelMappers.cs
@@ -5,6 +5,7 @@ namespace EuroMillions.API.Mappers;
 
 public static class StatisticViewModelMappers
 {
-    public static StatisticViewModel ToStatisticViewModel(this DrawItemStatisticsModel<DrawItemAbstraction> statistics) =>
+    public static StatisticViewModel ToStatisticViewModel<TDrawItem>(this DrawItemStatisticsModel<TDrawItem> statistics)
+        where TDrawItem : DrawItemAbstraction =>
         new StatisticViewModel {DrawItem = statistics.DrawItem, NbTimesDraw = statistics.NbTimesDraw};
 }
