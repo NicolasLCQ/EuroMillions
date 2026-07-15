2303ae53091e5ec305fe320f61540b4473f51db7
NicolasLCQ <pro.nicolasleclercq@gmail.com>
2026-07-15 02:44:11 +0200

    refactor(mappers): unify ToStatisticViewModel implementations using generic DrawItemStatisticsModel

--- server/EuroMillions.API/Mappers/StatisticViewModelMappers.cs
+++ server/EuroMillions.API/Mappers/StatisticViewModelMappers.cs
@@ -5,17 +5,6 @@ namespace EuroMillions.API.Mappers;
 
 public static class StatisticViewModelMappers
 {
-    public static StatisticViewModel ToStatisticViewModel(this DrawItemStatisticsModel<Ball> statistics) =>
-        new StatisticViewModel
-        {
-            DrawItem = statistics.DrawItem,
-            NbTimesDraw = statistics.NbTimesDraw
-        };
-
-    public static StatisticViewModel ToStatisticViewModel(this DrawItemStatisticsModel<Star> statistics) =>
-        new StatisticViewModel
-        {
-            DrawItem = statistics.DrawItem,
-            NbTimesDraw = statistics.NbTimesDraw
-        };
+    public static StatisticViewModel ToStatisticViewModel(this DrawItemStatisticsModel<DrawItemAbstraction> statistics) =>
+        new StatisticViewModel {DrawItem = statistics.DrawItem, NbTimesDraw = statistics.NbTimesDraw};
 }
--- server/EuroMillions.Application/Models/DrawItemStatisticsModel.cs
+++ server/EuroMillions.Application/Models/DrawItemStatisticsModel.cs
@@ -1,6 +1,7 @@
 namespace EuroMillions.Application.Models;
 
 public class DrawItemStatisticsModel<TDrawItem>
+    where TDrawItem : DrawItemAbstraction
 {
     public required TDrawItem DrawItem { get; set; }
     public int NbTimesDraw { get; set; }
