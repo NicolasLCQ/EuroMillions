ce8704e44d1cc9f788989bb51d2be40f4b1c62ce
NicolasLCQ <pro.nicolasleclercq@gmail.com>
2026-08-10 15:54:16 +0200

    refactor(extensions): revise `MinimalDrawModelExtensions` to use nested methods, improving readability and structure of draw statistics logic

--- server/EuroMillions.Application/Extensions/MinimalDrawModelExtensions.cs
+++ server/EuroMillions.Application/Extensions/MinimalDrawModelExtensions.cs
@@ -4,41 +4,47 @@ namespace EuroMillions.Application.Extensions;
 
 public static class MinimalDrawModelExtensions
 {
-    public static (BallDictionary Balls, StarDictionary Stars) CalculateNbTimesDraw(this IEnumerable<MinimalDrawModel> draws)
+    extension(IEnumerable<MinimalDrawModel> draws)
     {
-        BallDictionary ballDictionary = new BallDictionary();
-        StarDictionary starDictionary = new StarDictionary();
-
-        foreach (MinimalDrawModel draw in draws)
+        public (BallDictionary Balls, StarDictionary Stars) CalculateNbTimesDraw()
         {
-            ballDictionary.Increment([draw.Ball1, draw.Ball2, draw.Ball3, draw.Ball4, draw.Ball5]);
-            starDictionary.Increment([draw.Star1, draw.Star2]);
+            BallDictionary ballDictionary = new BallDictionary();
+            StarDictionary starDictionary = new StarDictionary();
+
+            foreach (MinimalDrawModel draw in draws)
+            {
+                ballDictionary.Increment([draw.Ball1, draw.Ball2, draw.Ball3, draw.Ball4, draw.Ball5]);
+                starDictionary.Increment([draw.Star1, draw.Star2]);
+            }
+
+            return (ballDictionary, starDictionary);
         }
 
-        return (ballDictionary, starDictionary);
-    }
-
-    public static (BallDictionary Balls, StarDictionary Stars) CalculateNbDrawsSinceLastOccurrence(
-        this IEnumerable<MinimalDrawModel> draws
-    )
-    {
-        BallDictionary ballDictionary = new BallDictionary();
-        StarDictionary starDictionary = new StarDictionary();
-        HashSet<Ball> ballsWithoutOccurrence = ballDictionary.Keys.ToHashSet();
-        HashSet<Star> starsWithoutOccurrence = starDictionary.Keys.ToHashSet();
-
-        foreach (MinimalDrawModel draw in draws)
+        public (BallDictionary Balls, StarDictionary Stars) CalculateNbDrawsSinceLastOccurrence()
         {
-            Ball[] drawnBalls = [draw.Ball1, draw.Ball2, draw.Ball3, draw.Ball4, draw.Ball5];
-            Star[] drawnStars = [draw.Star1, draw.Star2];
+            BallDictionary ballDictionary = new BallDictionary();
+            StarDictionary starDictionary = new StarDictionary();
+            HashSet<Ball> ballsWithoutOccurrence = ballDictionary.Keys.ToHashSet();
+            HashSet<Star> starsWithoutOccurrence = starDictionary.Keys.ToHashSet();
 
-            ballDictionary.Increment(ballsWithoutOccurrence.Except(drawnBalls));
-            starDictionary.Increment(starsWithoutOccurrence.Except(drawnStars));
+            foreach (MinimalDrawModel draw in draws)
+            {
+                Ball[] drawnBalls = [draw.Ball1, draw.Ball2, draw.Ball3, draw.Ball4, draw.Ball5];
+                Star[] drawnStars = [draw.Star1, draw.Star2];
 
-            ballsWithoutOccurrence.ExceptWith(drawnBalls);
-            starsWithoutOccurrence.ExceptWith(drawnStars);
+                ballsWithoutOccurrence.ExceptWith(drawnBalls);
+                starsWithoutOccurrence.ExceptWith(drawnStars);
+
+                if ((ballsWithoutOccurrence.Count == 0) && (starsWithoutOccurrence.Count == 0))
+                {
+                    break;
+                }
+
+                ballDictionary.Increment(ballsWithoutOccurrence);
+                starDictionary.Increment(starsWithoutOccurrence);
+            }
+
+            return (ballDictionary, starDictionary);
         }
-
-        return (ballDictionary, starDictionary);
     }
 }
